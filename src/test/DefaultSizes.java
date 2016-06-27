0c054b83119362c03df73d6084826cdcfce286fa
anupamadak <anupam5659@live.com>
2016-06-27 23:42:24 +0530

    Signed-off-by: anupamadak <anupam5659@live.com>

--- src/test/DefaultSizes.java
+++ src/test/DefaultSizes.java
@@ -1,8 +1,14 @@
 package test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class DefaultSizes {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		HashMap<String, Integer> map = new HashMap<>(16);
+		HashSet<String> tree = new HashSet<>();
 		System.out.println("anupam");
 
 	}
